7310895a95f1b32b537dd169f075a68ec5f9d2b3
CSenshi <saba.pochkhua@gmail.com>
2020-05-07 03:26:57 +0400

    remove unwanted imports from init pkg

--- validator/__parser__/parser.py
+++ validator/__parser__/parser.py
@@ -1,3 +1,6 @@
+import validator.rules as R
+
+
 class Parser:
     def __init__(self, rules):
         self.rules = rules
--- validator/rules/__init__.py
+++ validator/rules/__init__.py
@@ -13,8 +13,15 @@ for (_, file, _) in pkgutil.iter_modules([Path(__file__).parent]):
     # Get Absolute Path
     module_abs_path = f"validator.rules.{file}"
 
+    # add module to globals() (e.g. add 'min')
     pkg = importlib.import_module(module_abs_path)
 
     # Import all classes from given modules
     names = [x for x in pkg.__dict__ if not x.startswith("_")]
+
+    # add class from module to globals() adn all (e.g. add 'min.Min')
     __all__.update({k: getattr(pkg, k) for k in names})
+    globals().update({k: getattr(pkg, k) for k in names})
+
+    # remove module from globals (remove 'min' but leave 'Min')
+    del globals()[file]
