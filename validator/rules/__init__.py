405c7a920d2921602e34a4a9b9dea491b83b50a9
CSenshi <saba.pochkhua@gmail.com>
2020-05-07 03:09:41 +0400

    change attribute saving from global() to __all__

--- validator/rules/__init__.py
+++ validator/rules/__init__.py
@@ -8,6 +8,7 @@ Import all modules from Rules folder dynamicly
 """
 
 # Iterate each module in the given package
+__all__ = {}
 for (_, file, _) in pkgutil.iter_modules([Path(__file__).parent]):
     # Get Absolute Path
     module_abs_path = f"validator.rules.{file}"
@@ -16,4 +17,4 @@ for (_, file, _) in pkgutil.iter_modules([Path(__file__).parent]):
 
     # Import all classes from given modules
     names = [x for x in pkg.__dict__ if not x.startswith("_")]
-    globals().update({k: getattr(pkg, k) for k in names})
+    __all__.update({k: getattr(pkg, k) for k in names})
