924172eed8cae07e081d2f37054895a2a1d0288f
CSenshi <saba.pochkhua@gmail.com>
2020-05-07 18:33:59 +0400

    add required implementation + doctests

--- validator/rules/required.py
+++ validator/rules/required.py
@@ -2,8 +2,20 @@ from validator.rules import Rule
 
 
 class Required(Rule):
+    """
+    >>> TrueRequired = Required()
+    >>> TrueRequired('Not Empty')
+    True
+
+    >>> FalseRequired = Required()
+    >>> FalseRequired('')
+    False
+    """
+
     def __init__(self):
         pass
 
     def __call__(self, arg):
-        pass
+        if not arg:
+            return False
+        return True
