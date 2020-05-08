be68caf8aa8e00d45e42759e8d6e9a29624982af
beqakd <bgoga16@freeuni.edu.ge>
2020-05-08 22:25:57 +0400

    add more tests

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -12,12 +12,39 @@ def test_validator_001_simple():
     result = Validator(request, rule).validate()
     assert result == False
 
+    request = {"age": 13}
+    rule = {"age": [R.Max(18)]}
+    result = Validator(request, rule).validate()
+    assert result == True
+
+    request = {"name": "Jon"}
+    rule = {"name": [R.Required()]}
+    result = Validator(request, rule).validate()
+    assert result == True
+
+    request = {"name": ""}
+    rule = {"name": [R.Required()]}
+    result = Validator(request, rule).validate()
+    assert result == False
+
+
+def test_validator_002_simple():
+    request = {"age": 23}
+    rule = {"age": [R.Min(18), R.Max(30)]}
+    result = Validator(request, rule).validate()
+    assert result == True
+
+    request = {"age": 33}
+    rule = {"age": [R.Min(18), R.Max(30)]}
+    result = Validator(request, rule).validate()
+    assert result == False
+
     request = {"age": 23, "name": "Jon"}
     rule = {"age": [R.Min(18)], "name": [R.Required()]}
     result = Validator(request, rule).validate()
     assert result == True
 
     request = {"age": 23, "name": ""}
-    rule = {"age": [R.Min(18)], "name": [R.Required()]}
+    rule = {"age": [R.Min(18), R.Max(30)], "name": [R.Required()]}
     result = Validator(request, rule).validate()
     assert result == False
