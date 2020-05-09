c3947d1de3dd90989c1e7eb89945ba37c63056c2
Saba Pochkhua <saba.pochkhua@gmail.com>
2020-05-09 18:18:13 +0400

    remove rules.py

--- validator/rules.py
+++
@@ -1,6 +0,0 @@
-from validator.rules_src import Rule 
-
-from validator.rules_src.mail import Mail
-from validator.rules_src.max import Max
-from validator.rules_src.min import Min
-from validator.rules_src.required import Required
