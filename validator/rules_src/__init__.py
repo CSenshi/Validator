622c27a6ee6d02756aa11f82e188609c70351506
CSenshi <saba.pochkhua@gmail.com>
2020-05-11 02:13:54 +0400

    add __all__ dict to Rule import

--- validator/rules_src/__init__.py
+++ validator/rules_src/__init__.py
@@ -1,3 +1,9 @@
+import importlib
+import os
+import pkgutil
+from pathlib import Path
+
+
 class Rule:
     def __init__(self):
         pass
@@ -7,3 +13,19 @@ class Rule:
 
     def __from_str__(self):
         pass
+
+
+# Iterate each module in the given package and fill __all__ dictionary
+__all__ = {}
+for (_, file, _) in pkgutil.iter_modules([Path(__file__).parent]):
+    # Get Absolute Path
+    module_abs_path = f"validator.rules_src.{file}"
+
+    # import dynamicly given mo
+    pkg = importlib.import_module(module_abs_path)
+
+    # Import all classes from given modules
+    names = [x for x in pkg.__dict__ if not x.startswith("_")]
+
+    # add class from module to globals() adn all (e.g. add 'min.Min')
+    __all__.update({k: getattr(pkg, k) for k in names})
