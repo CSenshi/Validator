0b77896fa3335b439bd90650fc25a6722b0721d0
nikalosa <nlosa16@freeuni.edu.ge>
2020-05-12 16:16:24 +0400

    add between rule with tests

---
+++ tests/rules/test_between.py
@@ -0,0 +1,40 @@
+from validator.rules import Between
+
+
+def test_between_01():
+    rule = Between(2, 18)
+    value_to_check = 5
+    assert rule(value_to_check) == True
+
+    rule = Between(2, 18)
+    value_to_check = 1
+    assert rule(value_to_check) == False
+
+    rule = Between(2, 18)
+    value_to_check = 19
+    assert rule(value_to_check) == False
+
+
+def test_max_02():
+
+    rule = Between(-15, 30)
+    value_to_check = -15
+    assert rule(value_to_check) == True
+
+    rule = Between(-15, 30)
+    value_to_check = 30
+    assert rule(value_to_check) == True
+
+    rule = Between(-15, 30)
+    value_to_check = 0
+    assert rule(value_to_check) == True
+
+
+def test_max_03():
+    rule = Between(5, 5)
+    value_to_check = 5
+    assert rule(value_to_check) == True
+
+    rule = Between(5, 5)
+    value_to_check = 0
+    assert rule(value_to_check) == False
