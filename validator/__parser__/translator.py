3330f5147583b1cc9e7ad668ea7431e985c8477c
beqakd <bgoga16@freeuni.edu.ge>
2020-05-13 13:09:05 +0400

    Implemented some translator class functions.

--- validator/__parser__/translator.py
+++ validator/__parser__/translator.py
@@ -1,5 +1,8 @@
-from validator.rules import Rule
+from validator import rules as R
+import re
 
+# Some needed Variables
+target_char, target_regex = ':', '|'
 
 """
 Translator class is used by Parser class and implements translation
@@ -18,5 +21,26 @@ class Translator:
         self.value = value
 
     def translate(self):
-        # ToDo: Implement translation
-        return self.value
+        # First step. Split whole string to array.
+        mid_arr = re.split(r'[' + target_regex + ']', self.value)
+
+        print(mid_arr)
+
+        # Second step: Loop throught array and initialize class objects.
+        for class_str in mid_arr:
+            class_str = class_str.capitalize()
+
+            if target_char in class_str:
+                # Split by target character
+                target_arr = class_str.split(target_char)
+                class_name, class_arg = target_arr
+
+                # Initialize class
+                my_class = R.__all__[class_name](class_arg)
+                my_class.__from_str__()
+
+                print(my_class(25))  # Just for checking
+            else:
+                R.__all__[class_str]
+
+        return 0
