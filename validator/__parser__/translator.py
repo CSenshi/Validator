977208298eefbfea362555f09bb4b3e393856c30
CSenshi <saba.pochkhua@gmail.com>
2020-05-17 02:22:18 +0400

    code reformat + remove debug logs

--- validator/__parser__/translator.py
+++ validator/__parser__/translator.py
@@ -30,7 +30,6 @@ class Translator:
         # Second step: Loop throught array and initialize class objects.
         rules_arr = []
         for elem in arr:
-            print(elem, elem.__class__)
             if isinstance(elem, str):
                 rule = self._translate_str(elem)
             elif isinstance(elem, R.Rule):
@@ -39,7 +38,6 @@ class Translator:
                 # ToDo: throw error
                 continue
             rules_arr.append(rule)
-        print(rules_arr)
         return rules_arr
 
     def _value_to_array(self):
@@ -68,14 +66,14 @@ class Translator:
         if not class_str in R.__all__:
             # ToDo: Throw Exception
             return None
-        
+
         init_rule = R.__all__[class_str]
 
-        # Chechk for arguments count 
+        # Chechk for arguments count
         if not self._validate_args_count(init_rule, args):
             # ToDo: Throw Exception
             return None
-        
+
         rule_instance = init_rule(*args)
         rule_instance.__from_str__()
 
@@ -85,4 +83,3 @@ class Translator:
         a = inspect.getfullargspec(init_rule)
         # ToDo: check for positional arguments
         return len(args) + 1 == len(a.args)
-            
