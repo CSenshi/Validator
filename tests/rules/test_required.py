a1206aa41b3d07089772032f3e56dfd0696841cc
ioane margiani <margianiioane@gmail.com>
2020-05-18 23:09:56 +0400

    added tests for required rule

--- tests/rules/test_required.py
+++ tests/rules/test_required.py
@@ -1,2 +1,30 @@
-def test_required():
-    assert True
+from validator.rules import Required
+
+
+def test_required_01():
+    rule = Required()
+    assert not rule([])
+    assert not rule("")
+    assert not rule(None)
+    assert not rule({})
+
+
+def test_required_02():
+    rule = Required()
+    value_to_check = 0
+    assert rule(value_to_check)
+
+    value_to_check = False
+    assert rule(value_to_check)
+
+    value_to_check = "Not empty"
+    assert rule(value_to_check)
+
+    value_to_check = "სხვა ენა ვცადოთ?"
+    assert rule(value_to_check)
+
+    value_to_check = ["Something here"]
+    assert rule(value_to_check)
+
+    value_to_check = {"Still not empty"}
+    assert rule(value_to_check)
