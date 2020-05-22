7158fb82843cf98ba3d74390a4c8e0973a8fde0c
nikalosa <nlosa16@freeuni.edu.ge>
2020-05-22 17:10:35 +0400

    Shorten exception classes

--- validator/errors.py
+++ validator/errors.py
@@ -1,45 +1,13 @@
-
-
-
 # validate args count exception
 class ArgsCountError(Exception):
-    def __init__(self, *args):
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
-
-    def __str__(self):
-        if self.message:
-            return 'ArgsCountError, {} '.format(self.message)
-        else:
-            return 'ArgsCountError has been raised'
+    pass
 
 
-# No such a rule in rulles list exception 
+# No such a rule in rulles list exception
 class NoRuleError(Exception):
-    def __init__(self, *args):
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
+    pass
 
-    def __str__(self):
-        if self.message:
-            return 'NoRuleError, {} '.format(self.message)
-        else:
-            return 'NoRuleError has been raised'
 
 # Invalid arguments for translating exception
 class UnknownTranslatorArgError(Exception):
-    def __init__(self, *args):
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
-
-    def __str__(self):
-        if self.message:
-            return 'UnknownTranslatorArgError, {} '.format(self.message)
-        else:
-            return 'UnknownTranslatorArgError has been raised'
+    pass
