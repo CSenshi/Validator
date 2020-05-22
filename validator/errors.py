4f93923d7e9dccbf351d54b3f2f4b7eb17179488
nikalosa <nlosa16@freeuni.edu.ge>
2020-05-22 16:44:15 +0400

    Add NoRuleError exception

--- validator/errors.py
+++ validator/errors.py
@@ -16,3 +16,17 @@ class ArgsCountError(Exception):
             return 'ArgsCountError has been raised'
 
 
+# No such a rule in rulles list exception 
+class NoRuleError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return 'NoRuleError, {} '.format(self.message)
+        else:
+            return 'NoRuleError has been raised'
+
