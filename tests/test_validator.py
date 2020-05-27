3066695013588f716e07d88a9b9472b283c4577d
bnMikheili <39998190+bnMikheili@users.noreply.github.com>
2020-05-27 17:25:07 +0400

    Validate function (#32)
    * move validator sanity check to constructor
    
    * split get_errors from validation and implement validator wrapper 'validate' function
    
    * update validator test according to the changes in validator
    
    * add validate import in __init__.py
    
    * add tests for validate wrapper function
    
    * fix styling
    
    * small fix

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,4 +1,4 @@
-from validator import Validator, rules as R
+from validator import Validator, validate, rules as R
 
 
 def test_validator_001_simple():
@@ -10,7 +10,7 @@ def test_validator_001_simple():
     request = {"age": 13}
     rule = {"age": [R.Min(18)]}
     result = Validator(request, rule).validate()
-    assert not result[0]
+    assert not result
 
     request = {"age": 13}
     rule = {"age": [R.Max(18)]}
@@ -25,10 +25,37 @@ def test_validator_001_simple():
     request = {"name": ""}
     rule = {"name": [R.Required()]}
     result = Validator(request, rule).validate()
-    assert not result[0]
+    assert not result
 
 
-def test_validator_002_simple():
+def test_validate_002_simple():
+    request = {"age": 23}
+    rule = {"age": [R.Min(18)]}
+    result = validate(request, rule)
+    assert result
+
+    request = {"age": 13}
+    rule = {"age": [R.Min(18)]}
+    result = validate(request, rule)
+    assert not result
+
+    request = {"age": 13}
+    rule = {"age": [R.Max(18)]}
+    result = validate(request, rule)
+    assert result
+
+    request = {"name": "Jon"}
+    rule = {"name": [R.Required()]}
+    result = validate(request, rule)
+    assert result
+
+    request = {"name": ""}
+    rule = {"name": [R.Required()]}
+    result = validate(request, rule)
+    assert not result
+
+
+def test_validator_003_simple():
     request = {"age": 23}
     rule = {"age": [R.Min(18), R.Max(30)]}
     result = Validator(request, rule).validate()
@@ -37,7 +64,7 @@ def test_validator_002_simple():
     request = {"age": 33}
     rule = {"age": [R.Min(18), R.Max(30)]}
     result = Validator(request, rule).validate()
-    assert not result[0]
+    assert not result
 
     request = {"age": 23, "name": "Jon"}
     rule = {"age": [R.Min(18)], "name": [R.Required()]}
@@ -47,13 +74,37 @@ def test_validator_002_simple():
     request = {"age": 23, "name": ""}
     rule = {"age": [R.Min(18), R.Max(30)], "name": [R.Required()]}
     result = Validator(request, rule).validate()
-    assert not result[0]
+    assert not result
 
 
-def test_validator_003_error_msg():
+def test_validate_004_simple():
+    request = {"age": 23}
+    rule = {"age": [R.Min(18), R.Max(30)]}
+    result = validate(request, rule)
+    assert result
+
+    request = {"age": 33}
+    rule = {"age": [R.Min(18), R.Max(30)]}
+    result = validate(request, rule)
+    assert not result
+
+    request = {"age": 23, "name": "Jon"}
+    rule = {"age": [R.Min(18)], "name": [R.Required()]}
+    result = validate(request, rule)
+    assert result
+
+    request = {"age": 23, "name": ""}
+    rule = {"age": [R.Min(18), R.Max(30)], "name": [R.Required()]}
+    result = validate(request, rule)
+    assert not result
+
+
+def test_validator_005_error_msg():
     request = {"age": 100}
     rule = {"age": [R.Between(18, 90)]}
-    result, errors = Validator(request, rule).validate()
+    val = Validator(request, rule)
+    result = val.validate()
+    errors = val.get_error_messages()
     assert not result
     assert "age" in errors.keys()
     assert "Between" in errors["age"].keys()
@@ -64,7 +115,9 @@ def test_validator_003_error_msg():
         "name": [R.Required()],
         "surname": [R.Required(), R.Mail()],
     }
-    result, errors = Validator(request, rule).validate()
+    val = Validator(request, rule)
+    result = val.validate()
+    errors = val.get_error_messages()
 
     # Test General
     assert not result
@@ -77,7 +130,34 @@ def test_validator_003_error_msg():
     assert "Mail" in mail_err.keys()
 
 
-def test_validator_004_error_msg():
+def test_validator_006_error_msg():
+    request = {"age": 100}
+    rule = {"age": [R.Between(18, 90)]}
+    result, errors = validate(request, rule, return_errors=True)
+    assert not result
+    assert "age" in errors.keys()
+    assert "Between" in errors["age"].keys()
+
+    request = {"age": 28, "name": "John", "surname": "Krasinski"}
+    rule = {
+        "age": [R.Between(18, 50)],
+        "name": [R.Required()],
+        "surname": [R.Required(), R.Mail()],
+    }
+    result, errors = validate(request, rule, return_errors=True)
+
+    # Test General
+    assert not result
+    assert len(errors) == 1
+    assert "surname" in errors.keys()
+
+    # Test Error 1
+    mail_err = errors["surname"]
+    assert len(mail_err) == 1
+    assert "Mail" in mail_err.keys()
+
+
+def test_validator_007_error_msg():
     request = {
         "age": 53,
         "name": "Peter",
@@ -92,7 +172,50 @@ def test_validator_004_error_msg():
         "profession": [R.Required, R.Mail],
         "mail": [R.Required(), R.Mail()],
     }
-    result, errors = Validator(request, rule).validate()
+    val = Validator(request, rule)
+    result = val.validate()
+    errors = val.get_error_messages()
+
+    # Test General
+    assert not result
+    assert len(errors) == 3
+    assert "age" in errors
+    assert "profession" in errors
+    assert "mail" in errors
+
+    # Test Error 1
+    age_err = errors["age"]
+    assert len(age_err) == 1
+    assert "Between" in age_err
+
+    # Test Error 2
+    profession_err = errors["profession"]
+    assert len(profession_err) == 2
+    assert "Required" in profession_err
+    assert "Mail" in profession_err
+
+    # Test Error 3
+    mail_err = errors["mail"]
+    assert len(mail_err) == 1
+    assert "Mail" in mail_err
+
+
+def test_validator_007_error_msg():
+    request = {
+        "age": 53,
+        "name": "Peter",
+        "surname": "Griffin",
+        "profession": "",
+        "mail": "petergriffin.com",
+    }
+    rule = {
+        "age": [R.Between(18, 50), R.Required()],
+        "name": [R.Required()],
+        "surname": [R.Required()],
+        "profession": [R.Required, R.Mail],
+        "mail": [R.Required(), R.Mail()],
+    }
+    result, errors = validate(request, rule, return_errors=True)
 
     # Test General
     assert not result
--- validator/validator.py
+++ validator/validator.py
@@ -15,15 +15,15 @@ class Validator:
     def __init__(self, request, rules):
         self.request = request
         self.rules = Parser(rules).parse()
-
-    def validate(self):
         # check for internal error (incorrect rules)
         if not self.check_rules():
             raise exc.RulesFormatError
 
+        self.errors = {}
+
+    def validate(self):
         # prepare variables
         result = True
-        errors = {}
 
         # at this point all rules are being correctly passed
         for key in self.rules:
@@ -37,9 +37,13 @@ class Validator:
                     errors_on_key[rule.get_class_name()] = rule.get_error_message()
 
             if errors_on_key:
-                errors[key] = errors_on_key
+                self.errors[key] = errors_on_key
 
-        return result, errors
+        return result
+
+    def get_error_messages(self):
+        # return error messages logged on validation
+        return self.errors
 
     def check_rules(self):
         # check for rules' type (should be dictionary)
@@ -56,3 +60,27 @@ class Validator:
                     False
 
         return True
+
+
+def validate(req, rules, return_errors=False):
+    """ 
+    Validates request with given rules
+  
+    Parameters: 
+        req (dict): request
+        rules (dict): rules
+        return_errors (bool): True/False according the necessity of returning error messages (False by default)
+  
+    Returns: 
+        result (bool): the result of the validation (if return_errors parameter was False)
+        OR
+        (result, error_messages): pair of the validation result and error messages object (if return_errors was True)
+    """
+    val = Validator(req, rules)
+    result = val.validate()
+    if return_errors:
+        errors = val.get_error_messages()
+        # if return_errors was True return pair as a tuple
+        return (result, errors)
+    # return validation result
+    return result
