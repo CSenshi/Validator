67a0aa88a3c848cfad8fabde877bc5a5dad85a6b
CSenshi <saba.pochkhua@gmail.com>
2020-06-15 19:23:50 +0400

    start dynamic readme generator

---
+++ utils/generate_readme.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import pkgutil
+import importlib
+from pathlib import Path
+import sys
+import os
+
+validator_path = os.path.abspath(Path(__file__).parent.parent)
+sys.path += [validator_path]
+
+README_EXAMPLE = "utils/README_EXAMPLE.md"
+README = "README.md"
+KEY = "CD4A678E95173E4BE5E27E2C8169F"
+HTML_DIV_EXAMPLE = "utils/readme_rules_div_example.html"
+HTML_REF_EXAMPLE = "utils/readme_rule_ref_example.html"
+
+
+def writeRules():
+    div_str = open(HTML_DIV_EXAMPLE).read()
+    ref_str = open(HTML_REF_EXAMPLE).read()
+    # help(importlib.import_module)
+
+    for (_, file, _) in pkgutil.iter_modules(['validator/rules_src']):
+        # Get Absolute Path
+        module_abs_path = f"validator.rules_src.{file}"
+        pkg = importlib.import_module(module_abs_path)
+        print(pkg)
+
+
+with open(README_EXAMPLE, "r") as ex, open(README, "w") as re:
+    while True:
+        line = ex.readline()
+        if not line:
+            break
+        if KEY in line:
+            writeRules()
