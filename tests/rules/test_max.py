990129faafa6296377d3cdad7e7a6cc0c5bec55c
beqakd <39763019+beqakd@users.noreply.github.com>
2020-06-15 19:32:42 +0400

    Translator testing (#38)
    * add bad tests for translator.
    
    * style changes
    
    * add bad tests for rule classes

--- tests/rules/test_max.py
+++ tests/rules/test_max.py
@@ -1,4 +1,5 @@
 from validator.rules import Max
+import pytest
 
 
 def test_max_01():
@@ -41,3 +42,23 @@ def test_max_03():
     rule = Max(-23)
     value_to_check = -23
     assert rule.check(value_to_check)
+
+
+def test_max_bad():
+    # zero arg
+    with pytest.raises(TypeError):
+        rule = Max()
+        value_to_check = 0
+        assert not rule.check(value_to_check)
+
+    # many arg
+    with pytest.raises(TypeError):
+        rule = Max(5, 5, 5)
+        value_to_check = 0
+        assert not rule.check(value_to_check)
+
+    # wrong type
+    with pytest.raises(TypeError):
+        rule = Max("5", "5")
+        value_to_check = 0
+        assert not rule.check(value_to_check)
