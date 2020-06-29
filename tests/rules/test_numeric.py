55a4a379c17f26cca39a682b9df06da7195027f0
Benjamin Soyka <bensoyka@icloud.com>
2020-06-28 18:13:24 -0600

    Add a few extra tests

--- tests/rules/test_numeric.py
+++ tests/rules/test_numeric.py
@@ -6,6 +6,14 @@ def test_numeric_01():
 
     assert Numeric().check(123)
 
+    assert Numeric().check(1.23)
+
+    assert Numeric().check(3+1j)
+
+    assert Numeric().check("1.23")
+
+    assert Numeric().check("3+1j")
+
 
 def test_numeric_02():
     assert not Numeric().check("abc")
