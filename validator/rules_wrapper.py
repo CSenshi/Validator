bd15b97534aa7808e86ea7e79da497b195b9724b
iko1133 <margianiioane@gmail.com>
2020-07-04 00:23:00 +0400

    add rw file

---
+++ validator/rules_wrapper.py
@@ -0,0 +1,36 @@
+from .rule_pipe_validator import RulePipeValidator as RPV
+
+
+class RulesWrapper:
+    def __init__(self, request, rules):
+        self.request = request
+        self.rules = rules
+        self.errors = {}
+        self.result = False
+
+    def run(self):
+        # prepare variables
+        result = True
+
+        # at this point all rules are being correctly passed
+        for key in self.rules:
+            rules = self.rules[key]
+            data = self.request[key]
+
+            # Interface for rules
+            rpv = RPV(data, rules)
+            rpv_result = rpv.execute()
+            errors_on_key = rpv.get_error_messages()
+
+            # if current validation fails change final result
+            if not rpv_result:
+                result = rpv_result
+                self.errors[key] = errors_on_key
+
+        self.result = result
+
+    def get_errors(self):
+        return self.errors
+
+    def get_result(self):
+        return self.result
