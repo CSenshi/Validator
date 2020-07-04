cea54a3714c707c49ec7a497924f1350d957e13f
Benjamin Soyka <bensoyka@icloud.com>
2020-07-04 09:52:13 -0600

    Add more tests for the Regex rule

--- tests/rules/test_regex.py
+++ tests/rules/test_regex.py
@@ -10,6 +10,12 @@ def test_regex_01():
 
     assert Regex("abc*").check("ab")
 
+    assert Regex("gray|grey").check("gray")
+
+    assert Regex("z{3,6}").check("zzzz")
+
+    assert Regex("xyz$").check("abcxyz")
+
 
 def test_regex_02():
     assert not Regex("^c").check("abcdef")
@@ -17,3 +23,9 @@ def test_regex_02():
     assert not Regex("^foo$").check("foobar")
 
     assert not Regex("abc*").check("a")
+
+    assert not Regex("gr[ae]y").check("groy")
+
+    assert not Regex("z{3,6}").check("xyzzxy")
+
+    assert not Regex("xyz$").check("xyzabc")
