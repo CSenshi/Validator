9c16e947ac853ba2e20f49a94ef92509b6d27077
ioane margiani <margianiioane@gmail.com>
2020-07-12 20:37:33 +0400

    Add string tests for required rule

--- tests/rules/test_required.py
+++ tests/rules/test_required.py
@@ -1,4 +1,5 @@
 from validator.rules import Required
+from validator import validate
 
 
 def test_required_01():
@@ -23,3 +24,17 @@ def test_required_02():
     assert Required().check(["Something here"])
 
     assert Required().check({"Still not empty"})
+
+
+def test_required_03_string():
+    assert validate({"val": "abcdef"}, {"val": "required"})
+
+    assert validate({"val": 0}, {"val": "required"})
+
+    assert validate({"val": "15"}, {"val": "integer|required"})
+
+    assert not validate({"val": {}}, {"val": "required"})
+
+    assert not validate({"val": ""}, {"val": "required"})
+
+    assert not validate({"val": None}, {"val": "required"})
