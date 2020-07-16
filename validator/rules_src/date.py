24d76cad4343bc4eac825f6cb7b14f449cb27007
dbejanishvili <37870110+dbejanishvili@users.noreply.github.com>
2020-07-16 22:30:19 +0400

    New Rule: Date (#86)
    * Add new date validation rule
    
    * Add tests for date validation rule
    
    * Add dateutil library to requirements

---
+++ validator/rules_src/date.py
@@ -0,0 +1,35 @@
+from validator.rules_src import Rule
+from dateutil.parser import parse
+
+
+class Date(Rule):
+    """
+    The field under validation must be an Integer
+
+    Examples:
+    >>> from validator import validate
+
+    >>> reqs = {"date" : "25-08-1900"}
+    >>> rule = {"date" : "date"}
+    >>> validate(reqs, rule)
+    True
+
+    >>> reqs = {"date" : "32-12-2020"}
+    >>> rule = {"date" : "date"}
+    >>> validate(reqs, rule)
+    False
+    """
+
+    def __init__(self):
+        Rule.__init__(self)
+
+    def check(self, arg):
+        try:
+            resp = parse(arg)
+            return True
+        except ValueError as err:
+            self.set_errror_message(f"Invalid: {err}")
+        return False
+
+    def __from_str__(self):
+        pass
