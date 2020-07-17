60a700146ff65da6fbf624b5669d986d1edb332c
bakurits <bakuricucxashvili@gmail.com>
2020-07-17 13:57:32 +0400

    added tests for json array

--- tests/rules/test_json.py
+++ tests/rules/test_json.py
@@ -8,6 +8,14 @@ def test_json_01():
 
     assert JSON().check('{"age":100 }')
 
+    assert JSON().check(
+        '[ {"name":"Ram", "email":"Ram@gmail.com"},  {"name":"Bob", "email":"bob32@gmail.com"}]'
+    )
+
+    assert JSON().check(
+        '{"data" : [ {"name":"Ram", "email":"Ram@gmail.com"},  {"name":"Bob", "email":"bob32@gmail.com"}]}'
+    )
+
     assert JSON().check('{"age":100 }')
 
 
@@ -17,5 +25,13 @@ def test_json_02():
     assert not JSON().check("{'age':100 }")
 
     assert not JSON().check("nawuteny-6603@com")
+
+    assert not JSON().check(
+        '{[ {"name":"Ram", "email":"Ram@gmail.com"},  {"name":"Bob", "email":"bob32@gmail.com"}]}'
+    )
+
+    assert not JSON().check(
+        '{data : [ {"name":"Ram", "email":"Ram@gmail.com"},  {"name":"Bob", "email":"bob32@gmail.com"}]}'
+    )
 
     assert not JSON().check([1, 3, 3])
