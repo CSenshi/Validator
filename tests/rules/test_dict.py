86cbf55a28323514c0c014196dee7673b4a8d967
negnatashvili <negnatashvili@bog.ge>
2020-07-19 23:02:49 +0400

    Added new rules in vaidator

---
+++ tests/rules/test_dict.py
@@ -0,0 +1,26 @@
+from validator.rules import Dict
+from validator import validate
+
+
+def test_dict_01():
+    assert Dict().check({"key_1": "val_1"})
+
+    assert Dict().check({})
+
+    assert Dict().check({1: 1, 2: 4, 3: 9})
+
+    assert Dict().check({"a": 1, "b": 2})
+
+    assert Dict().check({"": 0, "ab": 2})
+
+
+def test_dict_02():
+    assert not Dict().check(None)
+
+    assert not Dict().check([1, 2, 3])
+
+    assert not Dict().check({1, 2, 3})
+
+    assert not Dict().check("ASD")
+
+    assert not Dict().check("{a,s,d}")
