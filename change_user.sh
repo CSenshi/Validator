5563f0778e165d4ed4789a81e42466b680528aa7
bnMikheili <mnazg16@freeuni.edu.ge>
2020-07-20 16:47:11 +0400

    minor bug fix

--- change_user.sh
+++
@@ -1,8 +0,0 @@
-USER_NAME="bnMikheili"
-MAIL="mnazg16@freeuni.edu.ge"
-
-git config user.email ${MAIL}
-git config --global user.email ${MAIL}
-
-git config user.name ${USER_NAME}
-git config --global user.name ${USER_NAME}
